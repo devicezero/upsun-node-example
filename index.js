71df981894ea7c909aac535040b0baf039b01666
devicezero <jonas.kroeger@platform.sh>
2024-06-06 10:06:19 +0200

    look ma, i changed code!

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('look ma, a code change!')
 })
 
 app.listen(port, () => {
